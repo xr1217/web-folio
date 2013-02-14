4ad3921c1123551fc55214658906fb573116df01
XR <chia.xiangrong@dhs.sg>
2013-02-15 01:20:55 +0800

    Web-Folio Assignment

---
+++ script.js
@@ -0,0 +1,54 @@
+function toggleAbout() {
+	var ele = document.getElementById("toggleAbout");
+	var text = document.getElementById("displayAbout");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+} 
+
+function toggleEdu() {
+	var ele = document.getElementById("toggleEdu");
+	var text = document.getElementById("displayEdu");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+} 
+
+function toggleInt() {
+	var ele = document.getElementById("toggleInt");
+	var text = document.getElementById("displayInt");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+} 
+
+function toggleContact() {
+	var ele = document.getElementById("toggleContact");
+	var text = document.getElementById("displayContact");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+} 
+
+function togglesrc() {
+	var ele = document.getElementById("togglesrc");
+	var text = document.getElementById("displaysrc");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+} 
